a9ab9e0a699b31d67e558b031ad3cea6ba478ea5
Abhishek Mane <113165693+abhishekmane6122@users.noreply.github.com>
2024-11-11 10:46:43 +0530

    Update start.sh

--- Model Deployment/start.sh
+++ Model Deployment/start.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Grant execute permissions for the startup script (needed for Render)
-chmod +x start.sh
-
-# Start the Streamlit app
-streamlit run main.py
+streamlit run main.py --server.port $PORT --server.enableCORS false
